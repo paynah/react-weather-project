12f90acea8606c1b788bd2eb64cb205505fec458
paynah <nanczilla@gmail.com>
2022-08-29 15:12:53 -0700

    Created new WeatherIcon component and updated CurrentWeatherTemps component accordingly

--- src/CurrentWeatherTemps.js
+++ src/CurrentWeatherTemps.js
@@ -1,47 +1,8 @@
 import React from "react";
 import "./CurrentWeatherTemps.css";
-import zero1D from "./images/01d.png";
-import zero1N from "./images/01n.png";
-import zero2D from "./images/02d.png";
-import zero2N from "./images/02n.png";
-import zero3D from "./images/03d.png";
-import zero3N from "./images/03n.png";
-import zero4D from "./images/04d.png";
-import zero4N from "./images/04n.png";
-import zero9D from "./images/09d.png";
-import zero9N from "./images/09n.png";
-import tenD from "./images/10d.png";
-import tenN from "./images/10n.png";
-import elevenD from "./images/11d.png";
-import elevenN from "./images/11n.png";
-import thirteenD from "./images/13d.png";
-import thirteenN from "./images/13n.png";
-import fiftyD from "./images/50d.png";
-import fiftyN from "./images/50n.png";
+import WeatherIcon from "./WeatherIcon";
 
 export default function CurrentWeatherTemps(props) {
-    const weatherIconMapping = {
-        "01d": zero1D,
-        "01n": zero1N,
-        "02d": zero2D,
-        "02n": zero2N,
-        "03d": zero3D,
-        "03n": zero3N,
-        "04d": zero4D,
-        "04n": zero4N,
-        "09d": zero9D,
-        "09n": zero9N,
-        "10d": tenD,
-        "10n": tenN,
-        "11d": elevenD,
-        "11n": elevenN,
-        "13d": thirteenD,
-        "13n": thirteenN,
-        "50d": fiftyD,
-        "50n": fiftyN
-    };
-    let weatherIcon = weatherIconMapping[props.weatherData.icon];
-
     let temp = props.weatherData.temperature;
     let tempMax = props.weatherData.tempMax;
     let tempMin = props.weatherData.tempMin;
@@ -54,7 +15,7 @@ export default function CurrentWeatherTemps(props) {
     return (
         <div className="row currentWeatherTemps">
             <div className="col-md-auto">
-            <img src={weatherIcon} alt="" id="big-weather-icon" />
+            <WeatherIcon iconName={props.weatherData.icon} iconSize="large" />
             </div>
             <div className="col temps">
             <div className="row">
---
+++ src/WeatherIcon.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./WeatherIcon.css";
+import zero1D from "./images/01d.png";
+import zero1N from "./images/01n.png";
+import zero2D from "./images/02d.png";
+import zero2N from "./images/02n.png";
+import zero3D from "./images/03d.png";
+import zero3N from "./images/03n.png";
+import zero4D from "./images/04d.png";
+import zero4N from "./images/04n.png";
+import zero9D from "./images/09d.png";
+import zero9N from "./images/09n.png";
+import tenD from "./images/10d.png";
+import tenN from "./images/10n.png";
+import elevenD from "./images/11d.png";
+import elevenN from "./images/11n.png";
+import thirteenD from "./images/13d.png";
+import thirteenN from "./images/13n.png";
+import fiftyD from "./images/50d.png";
+import fiftyN from "./images/50n.png";
+
+export default function WeatherIcon(props) {
+    const weatherIconMapping = {
+        "01d": zero1D,
+        "01n": zero1N,
+        "02d": zero2D,
+        "02n": zero2N,
+        "03d": zero3D,
+        "03n": zero3N,
+        "04d": zero4D,
+        "04n": zero4N,
+        "09d": zero9D,
+        "09n": zero9N,
+        "10d": tenD,
+        "10n": tenN,
+        "11d": elevenD,
+        "11n": elevenN,
+        "13d": thirteenD,
+        "13n": thirteenN,
+        "50d": fiftyD,
+        "50n": fiftyN
+    };
+    let weatherIcon = weatherIconMapping[props.iconName];
+
+    if (props.iconSize === "large") {
+        return (
+            <img src={weatherIcon} alt="" id="big-weather-icon" />
+        );
+    } else {
+        return (
+            <div>
+                <img src={weatherIcon} alt="" class="dailyWeatherIcon" />
+            </div>
+        );
+    }
+}
